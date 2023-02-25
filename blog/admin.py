550734baf30e720a3799129b2010f1baf8d486b0
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2023-02-25 20:28:55 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 # Register your models here.
 
 admin.site.register(Tag)
+admin.site.register(Comment)
 
 
 @admin.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
 from django.conf import settings
 # Create your models here.
 
@@ -8,6 +11,17 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Comment(models.Model):
+  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey("content_type", "object_id")
+
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -19,6 +33,7 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
